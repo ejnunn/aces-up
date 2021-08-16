276fc1f2b1ae679ad134c47c0b3d5af2487aad61
pabi <dhaliwalpara@seattleu.edu>
2021-08-16 12:27:50 -0700

    Added method outlines to Deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,20 @@
 import java.util.ArrayList;
 
 public class Deck {
-    ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    ArrayList<Card> deckOfCards;
+
+    public Deck(){
+        this.deckOfCards = new ArrayList<Card>();
+        generateDeck();
+        shuffleDeck();
+    }
+
+    private void generateDeck() {
+    }
+
+    private void shuffleDeck() {
+    }
+
+    
 
 }
