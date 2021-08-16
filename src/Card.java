bb91f760a1727edb88ec2757de400e8fef2f3271
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-08-16 12:10:34 -0700

    Converted suit and rank to enums and created constructor and getter functions

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,27 @@
 public class Card {
 
-    private String suit;
-    private String faceValue;
-    
-    public static void main(String[] args) {
-        
+    private enum Suit {
+        HEART, DIAMOND, CLUB, SPADE;
     }
+
+    private enum Rank {
+        _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13;
+    }
+
+    private Suit suit;
+    private Rank rank;
+    
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Rank getRank() {
+        return this.rank;
+    }
+
 }
