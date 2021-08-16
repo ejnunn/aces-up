47327497df69c654894591a0117b9d2e842265d0
pabi <dhaliwalpara@seattleu.edu>
2021-08-16 12:46:30 -0700

    Implmented generateDeck method. Loops though all suit enums and creates 13 cards from rank 1-13.

--- src/Deck.java
+++ src/Deck.java
@@ -1,7 +1,10 @@
+import enums.*;
 import java.util.ArrayList;
 
 public class Deck {
     ArrayList<Card> deckOfCards;
+    int TOTALCARDSPERSUIT = 13;
+    int TOTALSUITS = 4;
 
     public Deck(){
         this.deckOfCards = new ArrayList<Card>();
@@ -10,6 +13,12 @@ public class Deck {
     }
 
     private void generateDeck() {
+        for (Suit s : Suit.values()){
+            for (int i = 0; i < TOTALCARDSPERSUIT; i ++){
+                Card tempCard = new Card (s, i + 1);
+                deckOfCards.add(tempCard);
+            }
+        }
     }
 
     private void shuffleDeck() {
