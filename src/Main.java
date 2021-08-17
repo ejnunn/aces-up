3173b391be8d684da5c08cd41898ff8fbcb724a3
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-08-17 12:34:21 -0700

    renamed application main file 'AcesUp' to 'Main' to avoid confusion

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner userInput = new Scanner(System.in);
+
+
+    public static void main(String[] args){
+        System.out.println("Let's play Aces Up!");
+        
+        AcesUpEngine gameEngine = new AcesUpEngine();
+        // gameEngine.startGame();
+        String junk = userInput.nextLine();
+
+        System.out.print("\033[H\033[2J"); // clears terminal screen
+        
+    }
+}
