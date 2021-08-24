a08e266a41c9454c1cd8aeb1898b6b6a01f4f367
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-08-23 21:13:05 -0700

    Added gameBoard to be a class attribute of AcesUpEngine. It is required to play the game. Other possible attributes could include deck, discard pile, player profile, scoreboard, ruleset...

--- src/main/AcesUpEngine.java
+++ src/main/AcesUpEngine.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class AcesUpEngine {
-
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner;
+    Board gameBoard;
     
+    public AcesUpEngine() {
+        scanner = new Scanner(System.in);
+        gameBoard = new Board();
+    }
+
     /**
      * Show the game menu and get a selection from the player.
      * @return String playerAction - Index of action item from the menu list
      */
-    public String getActionFromMenu() {
+    private String getActionFromMenu() {
         String playerAction;
 
         showMenu();
@@ -18,10 +23,31 @@ public class AcesUpEngine {
         return playerAction;
     }
 
-    private void showMenu() {
+    private void displayMenu() {
         System.out.println("1. Start new game.");
         System.out.println("2. Show rules.");
         System.out.println("3. Quit.");
     }
+
+    public void startGame() {
     
+        displayMenu();
+
+        String currentPlayerAction = getActionFromMenu();
+
+        if (currentPlayerAction == "Show Ruleset") {
+            System.out.println("These are the rules.\n1. Four cards are dealt.\n2. Discard all cards that share a SUIT and have a smaller FACE VALUE.\n3. Continue dealing and discarding until the deck is empty.\n4. The game is won if only the four ACES are left on the board.");
+        }
+        
+
+        System.out.print("\033[H\033[2J"); // clears terminal screen
+        
+        this.gameBoard.addCardToStack(new Card(Suit.CLUB, 7), 0);
+
+        System.out.println(this.gameBoard.toString());
+        
+}
+    
+
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -16,23 +16,8 @@ public class Main {
         
         AcesUpEngine gameEngine = new AcesUpEngine();
 
-        System.out.println("Before");
-        String currentPlayerAction = gameEngine.getActionFromMenu();
-        System.out.println("After");
-
-        if (currentPlayerAction == "Show Ruleset") {
-            System.out.println("These are the rules.\n1. Four cards are dealt.\n2. Discard all cards that share a SUIT and have a smaller FACE VALUE.\n3. Continue dealing and discarding until the deck is empty.\n4. The game is won if only the four ACES are left on the board.");
-        }
-        Board gameBoard = new Board();
+        gameEngine.startGame();
         
-        // gameEngine.startGame();
-        String junk = userInput.nextLine();
-
-        System.out.print("\033[H\033[2J"); // clears terminal screen
-        
-        gameBoard.addCardToStack(new Card(Suit.CLUB, 7), 0);
-
-        gameBoard.displayBoard();
-        
+        System.out.println("PROGRAM EXITING.");
     }
 }
