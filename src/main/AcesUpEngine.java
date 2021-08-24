321bd7bab64a1250308ad2326994cb960c75f3c1
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-08-23 20:59:51 -0700

    Adding menu for easier to understand user interface. Gets player input from keyboard to navigate the game environment.

--- src/main/AcesUpEngine.java
+++ src/main/AcesUpEngine.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class AcesUpEngine {
+
+    Scanner scanner = new Scanner(System.in);
+    
+    /**
+     * Show the game menu and get a selection from the player.
+     * @return String playerAction - Index of action item from the menu list
+     */
+    public String getActionFromMenu() {
+        String playerAction;
+
+        showMenu();
+
+        playerAction = scanner.nextLine(); // process holds until user input is validated
+
+        return playerAction;
+    }
+
+    private void showMenu() {
+        System.out.println("1. Start new game.");
+        System.out.println("2. Show rules.");
+        System.out.println("3. Quit.");
+    }
     
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,23 @@ public class Main {
 
     private static Scanner userInput = new Scanner(System.in);
 
-
+    /**
+     * Starts a new game of Aces Up.
+     * 
+     * @param args
+     */
     public static void main(String[] args){
         System.out.println("Let's play Aces Up!");
         
         AcesUpEngine gameEngine = new AcesUpEngine();
+
+        System.out.println("Before");
+        String currentPlayerAction = gameEngine.getActionFromMenu();
+        System.out.println("After");
+
+        if (currentPlayerAction == "Show Ruleset") {
+            System.out.println("These are the rules.\n1. Four cards are dealt.\n2. Discard all cards that share a SUIT and have a smaller FACE VALUE.\n3. Continue dealing and discarding until the deck is empty.\n4. The game is won if only the four ACES are left on the board.");
+        }
         Board gameBoard = new Board();
         
         // gameEngine.startGame();
